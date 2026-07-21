b4acca703db6b06f0f35ecfa3e7df104eae40e49
24kHandsome1201 <58929780+24kHandsome1201@users.noreply.github.com>
2026-07-21 23:29:15 +0800

    chore: prepare v1.16.0 release

--- Casks/tunara.rb
+++ Casks/tunara.rb
@@ -1,5 +1,5 @@
 cask "tunara" do
-  version "1.15.0"
+  version "1.16.0"
   sha256 "9269e41450b30635e69c833443bf97865b82c3b2732061661e5122270cac03d2"
 
   url "https://github.com/24kHandsome1201/tunara/releases/download/v#{version}/Tunara_#{version}_aarch64.dmg"
